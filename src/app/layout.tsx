156052ca4771ba4c9c31ad97cab4423e2cd2e2ac
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-06-28 12:28:25 +0300

    Update the template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { PropsWithChildren } from 'react';
 import type { Metadata } from 'next';
 
+import { Root } from '@/components/Root/Root';
+
 import '@telegram-apps/telegram-ui/dist/styles.css';
 import 'normalize.css/normalize.css';
-
 import './_assets/globals.css';
-import { Root } from '@/components/Root/Root';
 
 export const metadata: Metadata = {
   title: 'Your Application Title Goes Here',
