3aa08126c6481441389f313fb48191f0e7d6d33b
MrBeanDev <34623886+mrbeandev@users.noreply.github.com>
2024-08-04 06:20:09 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	reactStrictMode: true,
+	async rewrites() {
+		return [
+			{
+				source: "/tonconnect-manifest.json",
+				destination: "/api/tonconnect-manifest"
+			}
+		];
+	}
+};
 
 export default nextConfig;
