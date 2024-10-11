51088233ce805985d78974e3a1649ff8ed8449b4
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2024-10-11 13:36:02 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import type { PropsWithChildren } from 'react';
 import type { Metadata } from 'next';
+import { getLocale } from 'next-intl/server';
 
 import { Root } from '@/components/Root/Root';
+import { I18nProvider } from '@/core/i18n/provider';
 
 import '@telegram-apps/telegram-ui/dist/styles.css';
 import 'normalize.css/normalize.css';
 import './_assets/globals.css';
-import { I18nProvider } from '@/core/i18n/provider';
-import { getLocale } from 'next-intl/server';
 
 export const metadata: Metadata = {
   title: 'Your Application Title Goes Here',
